de71a6c7e3848d6a6883a42e03b56024027cd26a
Rizaldo Abdulrachman <rizaldo.a.r@gmail.com>
2021-03-10 04:37:51 +0700

    Second Commit

---
+++ src/ScoreBoard.func.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, button } from 'react-native'
+
+cimport React, { useState } from 'react';
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+ <view>
+   <text>Hello world</text>
+ </view>
+}
+
+export default App
